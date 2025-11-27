b25f5eb664286c9780375ba4448e34669c5fc4f3
Moka364mz <147619148+hFridayy@users.noreply.github.com>
2025-11-27 21:19:13 +0500

    update!

--- linkedlists/ReverseLinkedList.java
+++ linkedlists/ReverseLinkedList.java
@@ -1,5 +1,14 @@
 package linkedlists;
 
 public class ReverseLinkedList {
-    
+    public Node reverse(Node head){
+
+        Node curr = head;
+        Node prev = null;
+
+        while(curr != null){
+            
+        }
+        
+    }
 }
