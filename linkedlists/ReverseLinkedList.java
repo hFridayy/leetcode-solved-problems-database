667f93647f35749784d41375e756688c0c828cba
Moka364mz <147619148+hFridayy@users.noreply.github.com>
2025-11-27 21:48:32 +0500

    another update?

--- linkedlists/ReverseLinkedList.java
+++ linkedlists/ReverseLinkedList.java
@@ -7,8 +7,12 @@ public class ReverseLinkedList {
         Node prev = null;
 
         while(curr != null){
-            
+
+            Node nextNode = curr.next; //save the next position
+            curr.next = prev; //change the pointer
+            curr = nextNode;
+            prev = curr; //move pounters 
         }
-        
+        return prev;
     }
 }
